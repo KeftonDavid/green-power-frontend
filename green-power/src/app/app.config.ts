eee521a59a4a513be98fdfdf1569c8dfbd7e37c4
KeftonDavid <keftondavid@gmail.com>
2024-04-03 17:51:53 -0300

    feat: scroll position restoration to the top on routing

--- green-power/src/app/app.config.ts
+++ green-power/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
+const scroll: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled'
+};
+
+const scrollFeature: InMemoryScrollingFeature = withInMemoryScrolling(scroll);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes, scrollFeature)]
 };
