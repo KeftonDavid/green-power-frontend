ca41d1559db7a1ab2931552748e4897b9d6dec1f
KeftonDavid <keftondavid@gmail.com>
2024-04-03 17:11:57 -0300

    feat: contact component

--- green-power/src/app/app.routes.ts
+++ green-power/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     { component: HomeComponent, path: '' },
     { component: AboutComponent, path: 'about' },
-    { component: ServicesComponent, path: 'services' }
+    { component: ServicesComponent, path: 'services' },
+    { component: ContactComponent, path: 'contact' }
 ];
