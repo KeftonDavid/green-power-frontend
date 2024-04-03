3dbae12780a982fc02c7860d67bf1f5f379ca63a
KeftonDavid <keftondavid@gmail.com>
2024-04-03 17:41:24 -0300

    feat: navbar routing

--- green-power/src/app/navbar/navbar.component.ts
+++ green-power/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, RouterLink, RouterLinkActive, RouterOutlet, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
