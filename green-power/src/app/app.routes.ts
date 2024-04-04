7d528f90ad1d4e51e1b4b7c75b4c24a35bec860e
KeftonDavid <keftondavid@gmail.com>
2024-04-03 22:28:59 -0300

    feat: privacy policy component

--- green-power/src/app/app.routes.ts
+++ green-power/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'services', component: ServicesComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'privacy', component: PrivacyPolicyComponent }
 ];
