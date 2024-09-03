efa54cc0fef8b5efb12173dc5f4e4a8879a9f8ef
KeftonDavid <keftondavid@gmail.com>
2024-09-03 18:06:58 -0300

    feat: animated home page texts

--- green-power/src/app/home/home.component.ts
+++ green-power/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,7 +8,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   text!: string;
   words: string[] = ["O&M", "MANUTENÇÕES PREVENTIVAS", "MANUTENÇÕES CORRETIVAS", "CONSULTORIA"];
@@ -18,6 +18,28 @@ export class HomeComponent implements OnInit {
     setInterval(() => {
       this.updateText();
     }, 1500)
+
+  }
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', function() {
+      const animatedText = document.getElementById('animated-text');
+      const fadeInText = document.getElementById('fade-in-text');
+      const animatedTextPosition = animatedText?.getBoundingClientRect().top;
+      const fadeInTextPosition = fadeInText?.getBoundingClientRect().top;
+
+      const screenPosition = window.innerHeight / 1.3;
+  
+      if (animatedTextPosition && (animatedTextPosition < screenPosition)) {
+        animatedText.classList.add('animate-slideInLeft');
+        animatedText.classList.remove('-translate-x-full')
+      }
+
+      if (fadeInTextPosition && (fadeInTextPosition < screenPosition)) {
+        fadeInText.classList.add('animate-fadeIn');
+        fadeInText.classList.remove('opacity-0')
+      }
+    });
   }
 
   updateText(){
