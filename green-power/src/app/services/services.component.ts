c1b097c6b4e9d088639802e68e6ea28e53e478ce
KeftonDavid <keftondavid@gmail.com>
2024-09-04 17:05:06 -0300

    feat: services background and texts

--- green-power/src/app/services/services.component.ts
+++ green-power/src/app/services/services.component.ts
@@ -13,17 +13,20 @@ export class ServicesComponent {
     let details = document.getElementsByClassName("details");
     let plus = document.getElementsByClassName("plus");
     let minus = document.getElementsByClassName("minus");
+    let cards = document.getElementsByClassName("card");
     let detail = details[index] as HTMLElement;
-
+    let card = cards[index] as HTMLElement;
     plus[index].classList.toggle("change");
     minus[index].classList.toggle("change");
   
 
     if(detail.style.maxHeight){
       detail.style.maxHeight = '';
+      card.style.marginBottom = '';
     }
     else{
       detail.style.maxHeight = detail.scrollHeight + "px";
+      card.style.marginBottom = '2rem'
     }
 
   }
